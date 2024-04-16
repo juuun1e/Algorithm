aa7502602ca0a702f53f92ccce11f7e1f1558e4b
juuun1e <151481791+juuun1e@users.noreply.github.com>
2024-04-16 20:41:58 +0900

    [Bronze V] Title: 구구단, Time: 160 ms, Memory: 16128 KB -BaekjoonHub

--- 백준/Bronze/2739. 구구단/구구단.java
+++ 백준/Bronze/2739. 구구단/구구단.java
@@ -1,9 +1,12 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-  public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
-    int n = sc.nextInt();
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int n = Integer.parseInt(br.readLine());
+
     for (int i = 1; i < 10; i++) {
       System.out.println(n+" * "+i+" = "+ (n*i) );
     }
