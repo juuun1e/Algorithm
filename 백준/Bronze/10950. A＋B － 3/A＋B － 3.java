93e251054c7b3ea055231006b5210787ff3f2490
juuun1e <151481791+juuun1e@users.noreply.github.com>
2024-04-17 21:31:36 +0900

    [Bronze V] Title: A+B - 3, Time: 128 ms, Memory: 14196 KB -BaekjoonHub

--- 백준/Bronze/10950. A＋B － 3/A＋B － 3.java
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.java
@@ -9,14 +9,17 @@ public class Main {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
     int T = Integer.parseInt(br.readLine());
+    int arr[] = new int[T]; // 예제 출력처럼 구현하기 위해 배열에 담자
 
     for (int i = 0; i < T; i++) {
 
       StringTokenizer st = new StringTokenizer(br.readLine(), " ");
       int A = Integer.parseInt(st.nextToken());
       int B = Integer.parseInt(st.nextToken());
-
-      System.out.println(A+B);
+      arr[i] = A + B; // i 길이 만큼 배열에 A+B값을 담아
+    }
+    for (int C : arr) {
+      System.out.println(C); // 출력
     }
 
   }
